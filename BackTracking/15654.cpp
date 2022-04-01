5ce561cc04b8a3198d5d1e782ec0f4c95e80083e
Daehoww <kdh2769@gmail.com>
2022-04-01 23:44:04 +0900

    15654

---
+++ BackTracking/15654.cpp
@@ -0,0 +1,36 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int n, m;
+int board[10];
+int arr[10];
+bool vis[10];
+
+void func(int k){
+    if (k == m){
+        for (int i = 0 ;i < m ; i ++){
+            cout << arr[i] << ' ';
+        }
+        cout << '\n';
+        return;
+    }
+
+    for (int i = 0; i < n; i++){
+        if(vis[i]) continue;
+        arr[k] = board[i];
+        vis[i] = 1;
+        func(k+1);
+        vis[i] = 0;
+    }
+}
+int main(){
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+    cin >> n >> m ;
+    for (int i = 0; i < n ; i ++){
+        cin >> board[i];
+    }
+    sort (board, board + n); //
+    func(0);
+    return 0;
+}
